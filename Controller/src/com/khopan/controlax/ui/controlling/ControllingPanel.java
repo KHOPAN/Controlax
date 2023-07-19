976b012525142c85aa3dcdfe73ccf2b68d021396
KHOPAN <puthipnakchan@gmail.com>
2023-07-19 17:23:31 +0700

    Improve Remote Controlling
    Added sub-action such as (Mouse Move, Mouse Press) for Mouse and Keyboard controlling.

--- Controller/src/com/khopan/controlax/ui/controlling/ControllingPanel.java
+++ Controller/src/com/khopan/controlax/ui/controlling/ControllingPanel.java
@@ -1,8 +1,8 @@
 package com.khopan.controlax.ui.controlling;
 
-import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
+import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
@@ -11,34 +11,72 @@ public class ControllingPanel extends JPanel {
 	private static final long serialVersionUID = -5323401463653633664L;
 
 	public JCheckBox mouseControlBox;
+	public JCheckBox mouseMoveBox;
+	public JCheckBox mousePressBox;
+	public JCheckBox mouseReleaseBox;
+	public JCheckBox mouseWheelMoveBox;
 	public JCheckBox keyboardControlBox;
-	public TypingZone typingZone;
+	public JCheckBox keyPressBox;
+	public JCheckBox keyReleaseBox;
 
 	public ControllingPanel() {
 		this.setBorder(new TitledBorder("Controlling"));
 		this.setLayout(new GridLayout(1, 2));
-		JPanel mouseKeyboardPanel = new JPanel();
-		mouseKeyboardPanel.setLayout(new GridLayout(2, 1));
 		JPanel mousePanel = new JPanel();
 		mousePanel.setBorder(new TitledBorder("Mouse"));
-		mousePanel.setLayout(new GridLayout(2, 1));
+		mousePanel.setLayout(new BoxLayout(mousePanel, BoxLayout.Y_AXIS));
 		this.mouseControlBox = new JCheckBox();
 		this.mouseControlBox.setText("Mouse Control");
+		this.mouseControlBox.addChangeListener(Event -> this.updateMouseControl());
 		mousePanel.add(this.mouseControlBox);
-		mouseKeyboardPanel.add(mousePanel);
+		this.mouseMoveBox = new JCheckBox();
+		this.mouseMoveBox.setText("Mouse Move");
+		this.mouseMoveBox.setSelected(true);
+		mousePanel.add(this.mouseMoveBox);
+		this.mousePressBox = new JCheckBox();
+		this.mousePressBox.setText("Mouse Press");
+		this.mousePressBox.setSelected(true);
+		mousePanel.add(this.mousePressBox);
+		this.mouseReleaseBox = new JCheckBox();
+		this.mouseReleaseBox.setText("Mouse Release");
+		this.mouseReleaseBox.setSelected(true);
+		mousePanel.add(this.mouseReleaseBox);
+		this.mouseWheelMoveBox = new JCheckBox();
+		this.mouseWheelMoveBox.setText("Mouse Wheel Move");
+		this.mouseWheelMoveBox.setSelected(true);
+		mousePanel.add(this.mouseWheelMoveBox);
+		this.add(mousePanel);
+		this.updateMouseControl();
 		JPanel keyboardPanel = new JPanel();
 		keyboardPanel.setBorder(new TitledBorder("Keyboard"));
-		keyboardPanel.setLayout(new GridLayout(2, 1));
+		keyboardPanel.setLayout(new BoxLayout(keyboardPanel, BoxLayout.Y_AXIS));
 		this.keyboardControlBox = new JCheckBox();
 		this.keyboardControlBox.setText("Keyboard Control");
+		this.keyboardControlBox.addChangeListener(Event -> this.updateKeyboardControl());
 		keyboardPanel.add(this.keyboardControlBox);
-		mouseKeyboardPanel.add(keyboardPanel);
-		this.add(mouseKeyboardPanel);
-		JPanel typingPanel = new JPanel();
-		typingPanel.setBorder(new TitledBorder("Typing Zone"));
-		typingPanel.setLayout(new BorderLayout());
-		this.typingZone = new TypingZone();
-		typingPanel.add(this.typingZone, BorderLayout.CENTER);
-		this.add(typingPanel);
+		this.keyPressBox = new JCheckBox();
+		this.keyPressBox.setText("Key Press");
+		this.keyPressBox.setSelected(true);
+		keyboardPanel.add(this.keyPressBox);
+		this.keyReleaseBox = new JCheckBox();
+		this.keyReleaseBox.setText("Key Release");
+		this.keyReleaseBox.setSelected(true);
+		keyboardPanel.add(this.keyReleaseBox);
+		this.add(keyboardPanel);
+		this.updateKeyboardControl();
+	}
+
+	private void updateMouseControl() {
+		boolean enable = this.mouseControlBox.isSelected();
+		this.mouseMoveBox.setEnabled(enable);
+		this.mousePressBox.setEnabled(enable);
+		this.mouseReleaseBox.setEnabled(enable);
+		this.mouseWheelMoveBox.setEnabled(enable);
+	}
+
+	private void updateKeyboardControl() {
+		boolean enable = this.keyboardControlBox.isSelected();
+		this.keyPressBox.setEnabled(enable);
+		this.keyReleaseBox.setEnabled(enable);
 	}
 }
--- Controller/src/com/khopan/controlax/ui/image/StreamRenderer.java
+++ Controller/src/com/khopan/controlax/ui/image/StreamRenderer.java
@@ -1,11 +1,14 @@
 package com.khopan.controlax.ui.image;
 
 import java.awt.Color;
-import java.awt.Component;
+import java.awt.Cursor;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.RenderingHints;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
@@ -15,12 +18,15 @@ import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 import java.awt.image.BufferedImage;
 
+import javax.swing.JTextPane;
+import javax.swing.SwingUtilities;
+
 import com.khopan.controlax.Controlax;
 import com.khopan.controlax.action.action.KeyboardAction;
 import com.khopan.controlax.action.action.MouseAction;
 import com.khopan.controlax.packet.HeaderedImagePacket;
 
-public class StreamRenderer extends Component {
+public class StreamRenderer extends JTextPane { // Literally no way of focusing without this
 	private static final long serialVersionUID = 841821419923595603L;
 
 	public static final byte STREAM_HEADER = 0x5F;
@@ -31,6 +37,7 @@ public class StreamRenderer extends Component {
 	private int height;
 	private BufferedImage image;
 	private Image renderingImage;
+	private boolean focused;
 
 	public StreamRenderer() {
 		this.listener = new Listener();
@@ -38,7 +45,9 @@ public class StreamRenderer extends Component {
 		this.addMouseMotionListener(this.listener);
 		this.addMouseWheelListener(this.listener);
 		this.addKeyListener(this.listener);
+		this.addFocusListener(this.listener);
 		this.setFocusable(true);
+		this.setCursor(Cursor.getDefaultCursor());
 	}
 
 	@SuppressWarnings("deprecation")
@@ -72,17 +81,26 @@ public class StreamRenderer extends Component {
 		Graphics2D.setColor(new Color(0x000000));
 		Graphics2D.fillRect(0, 0, this.width, this.height);
 		Graphics2D.drawImage(this.renderingImage, 0, 0, null);
+
+		if(this.focused && !Controlax.INSTANCE.window.screenshotPanel.fullscreen.isFullscreen) {
+			Graphics2D.setColor(new Color(0x00FF00));
+			Graphics2D.fillRect(0, 0, this.width, 1);
+			Graphics2D.fillRect(this.width - 1, 0, 1, this.height);
+			Graphics2D.fillRect(0, this.height - 1, this.width, 1);
+			Graphics2D.fillRect(0, 0, 1, this.height);
+		}
+
 		Graphics2D.dispose();
 	}
 
-	private class Listener implements MouseListener, MouseMotionListener, MouseWheelListener, KeyListener {
+	private class Listener implements MouseListener, MouseMotionListener, MouseWheelListener, KeyListener, FocusListener {
 		private boolean pressedF9;
 		private boolean pressedF10;
 		private boolean pressedF11;
 
 		@Override
 		public void mouseMoved(MouseEvent Event) {
-			if(Controlax.INSTANCE.window.controllingPanel.mouseControlBox.isSelected()) {
+			if(Controlax.INSTANCE.window.controllingPanel.mouseControlBox.isSelected() && Controlax.INSTANCE.window.controllingPanel.mouseMoveBox.isSelected()) {
 				Controlax.INSTANCE.processor.sendAction(MouseAction.getMouseMoved(this.getX(Event.getX()), this.getY(Event.getY())));
 			}
 		}
@@ -94,21 +112,21 @@ public class StreamRenderer extends Component {
 
 		@Override
 		public void mousePressed(MouseEvent Event) {
-			if(Controlax.INSTANCE.window.controllingPanel.mouseControlBox.isSelected()) {
-				Controlax.INSTANCE.processor.sendAction(MouseAction.getMousePressed(this.getX(Event.getX()), this.getY(Event.getY()), Event.getButton()));
+			if(Controlax.INSTANCE.window.controllingPanel.mouseControlBox.isSelected() && Controlax.INSTANCE.window.controllingPanel.mousePressBox.isSelected()) {
+				Controlax.INSTANCE.processor.sendAction(MouseAction.getMousePressed(this.getX(Event.getX()), this.getY(Event.getY()), this.getButton(Event)));
 			}
 		}
 
 		@Override
 		public void mouseReleased(MouseEvent Event) {
-			if(Controlax.INSTANCE.window.controllingPanel.mouseControlBox.isSelected()) {
-				Controlax.INSTANCE.processor.sendAction(MouseAction.getMouseReleased(this.getX(Event.getX()), this.getY(Event.getY()), Event.getButton()));
+			if(Controlax.INSTANCE.window.controllingPanel.mouseControlBox.isSelected() && Controlax.INSTANCE.window.controllingPanel.mouseReleaseBox.isSelected()) {
+				Controlax.INSTANCE.processor.sendAction(MouseAction.getMouseReleased(this.getX(Event.getX()), this.getY(Event.getY()), this.getButton(Event)));
 			}
 		}
 
 		@Override
 		public void mouseWheelMoved(MouseWheelEvent Event) {
-			if(Controlax.INSTANCE.window.controllingPanel.mouseControlBox.isSelected()) {
+			if(Controlax.INSTANCE.window.controllingPanel.mouseControlBox.isSelected() && Controlax.INSTANCE.window.controllingPanel.mouseWheelMoveBox.isSelected()) {
 				Controlax.INSTANCE.processor.sendAction(MouseAction.getMouseWheelMoved(Event.getPreciseWheelRotation()));
 			}
 		}
@@ -129,7 +147,7 @@ public class StreamRenderer extends Component {
 			} else if(code == KeyEvent.VK_F11) {
 				this.pressedF11 = true;
 			} else {
-				if(Controlax.INSTANCE.window.controllingPanel.keyboardControlBox.isSelected()) {
+				if(Controlax.INSTANCE.window.controllingPanel.keyboardControlBox.isSelected() && Controlax.INSTANCE.window.controllingPanel.keyPressBox.isSelected()) {
 					Controlax.INSTANCE.processor.sendAction(KeyboardAction.getKeyPressed(code));
 				}
 			}
@@ -155,7 +173,7 @@ public class StreamRenderer extends Component {
 					Controlax.INSTANCE.window.screenshotPanel.fullscreen.dispose();
 				}
 			} else {
-				if(Controlax.INSTANCE.window.controllingPanel.keyboardControlBox.isSelected()) {
+				if(Controlax.INSTANCE.window.controllingPanel.keyboardControlBox.isSelected() && Controlax.INSTANCE.window.controllingPanel.keyReleaseBox.isSelected()) {
 					Controlax.INSTANCE.processor.sendAction(KeyboardAction.getKeyReleased(code));
 				}
 			}
@@ -176,12 +194,38 @@ public class StreamRenderer extends Component {
 			this.mouseMoved(Event);
 		}
 
+		@Override
+		public void focusGained(FocusEvent Event) {
+			StreamRenderer.this.focused = true;
+			StreamRenderer.this.repaint();
+		}
+
+		@Override
+		public void focusLost(FocusEvent Event) {
+			StreamRenderer.this.focused = false;
+			StreamRenderer.this.repaint();
+		}
+
 		private double getX(int x) {
 			return ((double) x) / ((double) StreamRenderer.this.width);
 		}
 
 		private double getY(int y) {
 			return ((double) y) / ((double) StreamRenderer.this.height);
+		}
+
+		private int getButton(MouseEvent Event) {
+			int button = 0;
+
+			if(SwingUtilities.isLeftMouseButton(Event)) {
+				button |= InputEvent.BUTTON1_DOWN_MASK;
+			} else if(SwingUtilities.isMiddleMouseButton(Event)) {
+				button |= InputEvent.BUTTON2_DOWN_MASK;
+			} else if(SwingUtilities.isRightMouseButton(Event)) {
+				button |= InputEvent.BUTTON3_DOWN_MASK;
+			}
+
+			return button;
 		}
 	}
 }
